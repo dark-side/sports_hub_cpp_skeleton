082135b1af7be4f89d5f513f14106b29a4ab4412
Andrii Fedorenko <65275975+fedorena@users.noreply.github.com>
2025-04-28 15:43:55 +0300

    rm debuh

--- auth_service/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/afedor/work/plgrnd/cpp_dsktp_genai_plgrnd/auth_service
-# Build directory: /Users/afedor/work/plgrnd/cpp_dsktp_genai_plgrnd/auth_service/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TokenGeneratorTest "/Users/afedor/work/plgrnd/cpp_dsktp_genai_plgrnd/auth_service/cmake-build-debug/auth_service_tests")
-set_tests_properties(TokenGeneratorTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/afedor/work/plgrnd/cpp_dsktp_genai_plgrnd/auth_service/CMakeLists.txt;40;add_test;/Users/afedor/work/plgrnd/cpp_dsktp_genai_plgrnd/auth_service/CMakeLists.txt;0;")
